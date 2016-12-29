eb4bf65cdc212033602073ad0200de4166599c2b
Guillermo Bisheimer <gbisheimer@bys-control.com.ar>
2016-12-29 11:16:45 -0300

    Update docker-compose a version 1.9.0

--- install_docker.sh
+++ install_docker.sh
@@ -3,7 +3,7 @@
 curl -sSL https://get.docker.com/ | sh
 
 # Instalo Docker compose
-sudo su -c 'curl -L https://github.com/docker/compose/releases/download/1.8.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose'
+sudo su -c 'curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose'
 sudo su -c 'chmod +x /usr/local/bin/docker-compose'
 
 # Instalo utilidades
