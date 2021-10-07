4d5893521d51f6253523aba9c3067927abcdfce3
Guillermo Bisheimer <gbisheimer@bys-control.com.ar>
2021-10-07 13:04:26 -0300

    Add install options for docker engine

--- install_docker_x86.sh
+++ install_docker_x86.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 set -e
 
+function install_docker_apt() {
+	sudo apt-get install \
+		apt-transport-https \
+		ca-certificates \
+		curl \
+		gnupg \
+		lsb-release
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+	echo \
+	  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+	  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	sudo apt-get update
+	sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
+}
+
+function install_docker_snap() {
+	sudo snap install docker
+	sudo ln -s /snap/bin/docker /usr/bin/docker
+	sudo ln -s /snap/bin/docker-compose /usr/bin/docker-compose
+}
+
 NO_COLOR='\033[0m'
 OK_COLOR='\033[32;01m'
 ERROR_COLOR='\033[31;01m'
@@ -38,9 +62,15 @@ sudo apt-get install -y --no-install-recommends powerline fonts-powerline
 
 if ! which docker 1>/dev/null; then
 	echo -e "${WARN_COLOR}===== Installing docker =====${NO_COLOR}"
-        sudo snap install docker
-	sudo ln -s /snap/bin/docker /usr/bin/docker
-	sudo ln -s /snap/bin/docker-compose /usr/bin/docker-compose
+	
+	echo -e "\n${OK_COLOR}Elegir version"
+	select yn in "SNAP" "APT"; do
+	    case $yn in
+		SNAP ) install_docler_snap; break;;
+		APT ) install_docker_apt; break;;
+	    esac
+	done	
+       
 	sudo groupadd docker && sudo usermod -aG docker $USER
 fi
 
